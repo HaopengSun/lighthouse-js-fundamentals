b032f5954db1040b55f1b2ba325b92cefc783ea7
HaopengSun <sunhaopeng2016@gmail.com>
2020-11-29 16:04:10 +0000

    add map() method

--- forEach.js
+++ forEach.js
@@ -27,3 +27,31 @@ test.forEach(function(item, index){
     }    
 });
 console.log(test);
+
+// Using forEach() will not be useful if you want to permanently modify the original array. 
+// forEach() always returns undefined. 
+// However, creating a new array from an existing array is simple with the powerful map() method.
+var donuts = ["jelly donut", "chocolate donut", "glazed donut"];
+var improvedDonuts = donuts.map(function(donut) {
+  donut += " hole";
+  donut = donut.toUpperCase();
+  return donut;
+});
+console.log(donuts);
+console.log(improvedDonuts);
+
+// The map() method creates a new array with the results of calling a function for every array element.
+// The map() method calls the provided function once for each element in an array, in order.
+
+var bills = [50.23, 19.12, 34.01,
+    100.11, 12.15, 9.90, 29.11, 12.99,
+    10.00, 99.22, 102.20, 100.10, 6.77, 2.22
+];
+var totals = bills.map(function (element) {
+    element *= 1.15;
+    element = element.toFixed(2);
+    element = Number(element);
+
+    return element;
+});
+console.log(totals);
