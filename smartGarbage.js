eaabac37463fbc2ebe93bd88082614d66c770445
HaopengSun <sunhaopeng2016@gmail.com>
2020-11-30 02:34:14 +0000

    objects exercise

---
+++ smartGarbage.js
@@ -0,0 +1,14 @@
+function smartGarbage(trash, bins){
+    if (trash === 'waste'){
+        bins.waste += 1;
+    }
+    if (trash === 'recycling'){
+        bins.recycling += 1;
+    }
+    if (trash === 'compost'){
+        bins.compost += 1;
+    }
+    return bins;
+}
+
+console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 }));
